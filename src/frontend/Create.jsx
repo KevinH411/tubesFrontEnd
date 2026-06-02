c5cae197e42f2c0e272e785292a477932b9ef9c3
0fflines <6182201102@student.unpar.ac.id>
2026-06-02 18:37:05 +0700

    add redirect to create page

--- src/frontend/Create.jsx
+++ src/frontend/Create.jsx
@@ -2,23 +2,35 @@ import Header from "./Header";
 import { A } from "@solidjs/router";
 import "./css/create.css";
 
-export default function Create () { //:P
+export default function Create() { //:P
+
+    const navigate = useNavigate();
+
+    const storedUser = localStorage.getItem("currentUser");
+    const currentUser = storedUser ? JSON.parse(storedUser) : null;
+
+    // redirect if not logged in
+    if (!currentUser) {
+        navigate("/Login");
+        return null;
+    }
+
     return (
         <>
             <Header></Header>
             <div class="body">
                 <h1>Create A New Task</h1>
-                <div class = "container">
+                <div class="container">
                     <div id="p">
-                    <p>Title</p>
-                    <p>Description</p>
-                    <p>Deadline</p>
+                        <p>Title</p>
+                        <p>Description</p>
+                        <p>Deadline</p>
                     </div>
 
                     <div id="inputs">
-                    <input type="text" placeholder="Task name.."></input>
-                    <input type="text" placeholder="Task description.."></input>
-                    <input type="date"></input>
+                        <input type="text" placeholder="Task name.."></input>
+                        <input type="text" placeholder="Task description.."></input>
+                        <input type="date"></input>
                     </div>
                 </div>
                 <A href="/">Create Task</A>
