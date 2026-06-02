89a6ed5166408443de595365140344335b03151d
Jihan Aulia Syaharani <jihanaulia2021@gmail.com>
2026-06-02 18:45:39 +0700

    Update SignUp.jsx to add new users to database

--- src/frontend/SignUp.jsx
+++ src/frontend/SignUp.jsx
@@ -1,5 +1,7 @@
 import Header from "./Header";
 import { A, useNavigate } from '@solidjs/router';
+import { findUserByEmail, findUserByName, addUser } from "../backend/Database";
+//untuk cek jika username atau email sudah ada di database
 import './css/Login.css'; // Kita gunakan CSS yang sama dengan login agar cepat
 
 function SignUp() {
@@ -7,7 +9,33 @@ function SignUp() {
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    navigate('/login'); // Setelah daftar, diarahkan ke login
+    const formData = new FormData(e.target);
+    const inputUsername = formData.get("username").trim();
+    const inputEmail = formData.get("email").trim();
+    const inputPassword = formData.get("password");
+    
+    if (inputUsername === "" || inputEmail === "" || inputPassword === "") {
+        alert("Belum ada data yang dimasukkan.");
+        return;
+    }
+
+    let checkName = findUserByName(inputUsername);
+    if (checkName !== null) {
+        alert("Username sudah digunakan! Gunakan username lain.");
+        return;
+    }
+
+    let checkEmail = findUserByEmail(inputEmail);
+    if (checkEmail !== null) {
+        alert("Email sudah terdaftar! Gunakan email lain.");
+        return;
+    }
+
+    let success = addUser(inputUsername, inputEmail, inputPassword);
+    if(success) {
+        alert("Akun berhasil dibuat!");
+        navigate('/login'); // Setelah daftar, diarahkan ke login
+    }
   };
 
   return (
@@ -26,15 +54,15 @@ function SignUp() {
                 <form class="login-form" onSubmit={handleSignUp}>
                     <div class="input-group">
                         <label for="username">Username</label>
-                        <input type="text" id="username" placeholder="Username kamu" required />
+                        <input type="text" id="username" name="username" placeholder="Username kamu" required />
                     </div>
                     <div class="input-group">
                         <label for="email">Email</label>
-                        <input type="email" id="email" placeholder="nama@email.com" required />
+                        <input type="email" id="email" name="email" placeholder="nama@email.com" required />
                     </div>
                     <div class="input-group">
                         <label for="password">Password</label>
-                        <input type="password" id="password" placeholder="********" required />
+                        <input type="password" id="password" name="password" placeholder="********" required />
                     </div>
 
                     <button type="submit" class="login-btn">Daftar</button>
