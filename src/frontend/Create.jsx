e628bc6178adc7cae00a4b12070c1250f4dd6645
0fflines <6182201102@student.unpar.ac.id>
2026-06-02 18:38:46 +0700

    fix

--- src/frontend/Create.jsx
+++ src/frontend/Create.jsx
@@ -1,5 +1,5 @@
 import Header from "./Header";
-import { A } from "@solidjs/router";
+import { A, useNavigate } from "@solidjs/router";
 import "./css/create.css";
 
 export default function Create() { //:P
