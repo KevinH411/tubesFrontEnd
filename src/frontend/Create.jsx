08f4d0443140a9c3b51d4f08e54b872a002bd03f
bimoma <bimoadikoesoemo@gmail.com>
2026-06-09 15:50:03 +0700

    front end for signup and create todo

--- src/frontend/Create.jsx
+++ src/frontend/Create.jsx
@@ -1,6 +1,8 @@
 import Header from "./Header";
 import { A, useNavigate } from "@solidjs/router";
 import "./css/create.css";
+import { addTodo } from "./Database";
+import { createSignal } from "solid-js";
 
 export default function Create() { //:P
 
@@ -14,12 +16,25 @@ export default function Create() { //:P
         navigate("/Login");
         return null;
     }
+    const [title, setTitle] = createSignal("");
+    const [description, setDescription] = createSignal("");
+    const [dueDate, setDueDate] = createSignal("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await addTodo(title(), description(), dueDate());
+            navigate("/");
+        } catch (error) {
+            console.error("Error adding todo:", error);
+        }
+    };
 
     return (
         <>
             <Header></Header>
             <div class="body">
                 <h1>Create A New Task</h1>
+                <form onSubmit={handleSubmit}>
                 <div class="container">
                     <div id="p">
                         <p>Title</p>
@@ -28,13 +43,30 @@ export default function Create() { //:P
                     </div>
 
                     <div id="inputs">
-                        <input type="text" placeholder="Task name.."></input>
-                        <input type="text" placeholder="Task description.."></input>
-                        <input type="date"></input>
+                        <input type="text" 
+                            placeholder="Task title.."
+                            onInput={(e) => setTitle(e.target.value)} 
+                            value={title()}
+                            required
+                        />
+                        <input type="text" 
+                            placeholder="Task description.."
+                            onInput={(e) => setDescription(e.target.value)} 
+                            value={description()} 
+                            required                          
+                         />
+                        <input type="date" 
+                            onInput={(e) => setDueDate(e.target.value)} 
+                            value={dueDate()}
+                            required
+                         />
                     </div>
                 </div>
-                <A href="/">Create Task</A>
-                {/*untuk submit. untuk sekarang pakai A unutk direct ke home/mainpage*/}
+                    <button type="submit" class="create-task-btn">
+                        Create Task
+                    </button>
+                
+                </form>
             </div>
         </>
     );
